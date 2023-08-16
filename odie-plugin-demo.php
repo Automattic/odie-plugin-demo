a660681a69158f4d576863cfa41070bacac51141
dereksmart <smart@automattic.com>
2023-08-16 17:46:38 -0400

    fix updater

--- odie-plugin-demo.php
+++ odie-plugin-demo.php
@@ -125,7 +125,10 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 		'github_url' => 'https://github.com/Automattic/odie-plugin-demo', // the GitHub url of your GitHub repo
 		'zip_url' => 'https://github.com/Automattic/odie-plugin-demo/zipball/trunk', // the zip url of the GitHub repo
 		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+		'requires' => '3.0', // which version of WordPress does your plugin require?
+		'tested' => '6.3', // which version of WordPress is your plugin tested up to?
 		'readme' => 'README.md', // which file to use as the readme for the version number
+		'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
 	);
 	new WP_GitHub_Updater($config);
 }
